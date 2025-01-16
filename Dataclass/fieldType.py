807a31bbbdabd92d9ca8de935fbcf737bbc52eff
Joan DUSSAULD <joan.dussauld@etu.univ-nantes.fr>
2025-01-16 15:48:30 +0100

    init type eval

--- Dataclass/fieldType.py
+++ Dataclass/fieldType.py
@@ -1,8 +1,11 @@
 from enum import Enum
+from Dataclass.back.model import Model
+import re
+
 
 class FieldType(Enum):
     """
-    Enum pour représenter les types de champs.
+    Enum pour représenter les types primitifs.
     """
     STRING = "String"
     INT = "Int"
@@ -10,12 +13,91 @@ class FieldType(Enum):
     BOOLEAN = "Boolean"
     ID = "ID"
 
-    def compile_to_prisma(self):
-        match self.value:
-            case FieldType.ID.value:
-                return self.INT.value
-            case _:
-                return self.value
+
+class BaseType:
+    """
+    Classe de base pour tous les types.
+    """
+    def __init__(self, nullable: bool = True):
+        self.nullable = nullable
 
     def compile_to_graphql(self):
-        return self.value
+        raise NotImplementedError
+
+    def compile_to_prisma(self):
+        raise NotImplementedError
+
+
+class PrimitiveType(BaseType):
+    """
+    Représente un type primitif comme String, Int, etc.
+    """
+    def __init__(self, field_type: FieldType):
+        self.field_type = field_type
+
+    def compile_to_graphql(self):
+        return self.field_type.value
+
+    def compile_to_prisma(self):
+        return self.field_type.value
+
+
+class ModelType(BaseType):
+    """
+    Représente un type qui fait référence à un modèle.
+    """
+    def __init__(self, model: Model):
+        self.model = model
+
+    def compile_to_graphql(self):
+        return self.model.name
+
+    def compile_to_prisma(self):
+        return self.model.name
+
+class ListType(BaseType):
+    """
+    Représente un type liste.
+    """
+    def __init__(self, inner_type: BaseType):
+        self.inner_type = inner_type
+
+    def compile_to_graphql(self):
+        return f"[{self.inner_type.compile_to_graphql()}]"
+
+    def compile_to_prisma(self):
+        return f"{self.inner_type.compile_to_prisma()}[]"
+    
+
+def parse_graphql_type(type_string: str) -> BaseType:
+    """
+    Parse une chaîne de type GraphQL en un objet BaseType.
+    Exemple :
+      - `[Link!]!` -> ListType(ModelType("Link", nullable=False), nullable=False)
+      - `String!` -> PrimitiveType(FieldType.STRING, nullable=False)
+    """
+    # Supprimer les espaces inutiles
+    type_string = type_string.strip()
+
+    # Vérifier si le type est une liste : [Type]
+    if type_string.startswith('[') and type_string.endswith(']'):
+        inner_type_string = type_string[1:-1]  # Retirer les crochets
+        nullable = not type_string.endswith(']!')  # Vérifie si le type liste est nullable
+        inner_type = parse_graphql_type(inner_type_string)  # Appel récursif
+        return ListType(inner_type, nullable=nullable)
+
+    # Vérifier si le type est non nullable : Type!
+    if type_string.endswith('!'):
+        inner_type_string = type_string[:-1]  # Retirer le point d'exclamation
+        inner_type = parse_graphql_type(inner_type_string)  # Appel récursif
+        inner_type.nullable = False
+        return inner_type
+
+    # Sinon, c'est un type primitif ou un modèle
+    # Vérifier s'il s'agit d'un type primitif connu
+    for field_type in FieldType:
+        if type_string == field_type.value:
+            return PrimitiveType(field_type, nullable=True)
+
+    # Si ce n'est pas un type primitif, on suppose que c'est un modèle
+    return ModelType(type_string, nullable=True)
